df602c13c735a1e6bddb9c2ff75f6056bd8cfc85
kiku ishimoto <kikuish@gmail.com>
2020-05-06 20:02:31 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,8 +1,8 @@
 def prime?(n)
   start = 2
-  if n < 1
+  if n > 1
     range = (start..n-1).to_a
-    range.all?{ |num_to_test| n % num_to_test == 0}
+    range.none?{ |num_to_test| n % num_to_test == 0}
   else
     false
   end
